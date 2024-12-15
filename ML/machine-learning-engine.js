52814e17beaa95fc6c021265bb7c4bfcfbb4fc29
Habeebamoo <158174333+Habeebamoo@users.noreply.github.com>
2024-12-16 00:26:45 +0100

    Add files via upload

---
+++ ML/machine-learning-engine.js
@@ -0,0 +1,393 @@
+export const mle = [
+  {
+
+    keywords: 'hello',
+    reply: [
+      {
+        replyWord: 'Hi there!'
+      }, {
+        replyWord: 'How may i be of help'
+      }, {
+        replyWord: 'Listening...'
+      }, {
+        replyWord: 'What can i do for you today'
+      }
+    ]
+
+  }, {
+
+    keywords: 'Hello',
+    reply: [
+      {
+        replyWord: 'Hi there!'
+      }, {
+        replyWord: 'How may i be of help'
+      }, {
+        replyWord: 'Listening...'
+      }, {
+        replyWord: 'What can i do for you today'
+      }
+    ]
+
+  }, {
+
+    keywords: 'hi',
+    reply: [
+      {
+        replyWord: 'Hi there!'
+      }, {
+        replyWord: 'How may i be of help'
+      }, {
+        replyWord: 'Listening...'
+      }, {
+        replyWord: 'What can i do for you today'
+      }
+    ]
+
+  }, {
+
+    keywords: 'Hi',
+    reply: [
+      {
+        replyWord: 'Hi there!'
+      }, {
+        replyWord: 'How may i be of help'
+      }, {
+        replyWord: 'Listening...'
+      }, {
+        replyWord: 'What can i do for you today'
+      }
+    ]
+    
+  }, {
+
+    keywords: 'howfar',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'Howfar',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'afar',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'Afar',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'hafar',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'Hafar',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'awfar',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'Awfar',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'awfa',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'Awfa',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'how far',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'How far',
+    reply: [
+      {
+        replyWord: 'I am good, you?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'I am active boss'
+      }, {
+        replyWord: 'Yeah am good'
+      }
+    ]
+
+  }, {
+
+    keywords: 'hey',
+    reply: [
+      {
+        replyWord: 'yeah, how are you doing?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'Wassup friend'
+      }, {
+        replyWord: "What's up"
+      }
+    ]
+
+  }, {
+
+    keywords: 'Hey',
+    reply: [
+      {
+        replyWord: 'yeah, how are you doing?'
+      }, {
+        replyWord: 'Good, how may i be of your service'
+      }, {
+        replyWord: 'Wassup friend'
+      }, {
+        replyWord: "What's up"
+      }
+    ]
+
+  }, {
+
+    keywords: 'good morning',
+    reply: [
+      {
+        replyWord: 'Good Morning!, how are you doing?'
+      }, {
+        replyWord: 'Good Morning!, how can i assist you today'
+      }, {
+        replyWord: 'Good Morning!, how was your night'
+      }, {
+        replyWord: "Good Morning!, what can i do for you"
+      }
+    ]
+
+  }, {
+
+    keywords: 'Good morning',
+    reply: [
+      {
+        replyWord: 'Good Morning!, how are you doing?'
+      }, {
+        replyWord: 'Good Morning!, how can i assist you today'
+      }, {
+        replyWord: 'Good Morning!, how was your night'
+      }, {
+        replyWord: "Good Morning!, what can i do for you"
+      }
+    ]
+
+  }, {
+
+    keywords: 'good afternoon',
+    reply: [
+      {
+        replyWord: 'Afternoon!, how is the weather today?'
+      }, {
+        replyWord: 'Afternoon!, how can i assist you today'
+      }, {
+        replyWord: 'Good Afternoon!, how may i be of service'
+      }, {
+        replyWord: "Good Afternoon!, what can i do for you"
+      }
+    ]
+
+  }, {
+
+    keywords: 'Good afternoon',
+    reply: [
+      {
+        replyWord: 'Afternoon!, how is the weather today?'
+      }, {
+        replyWord: 'Afternoon!, how can i assist you today'
+      }, {
+        replyWord: 'Good Afternoon!, how may i be of service'
+      }, {
+        replyWord: "Good Afternoon!, what can i do for you"
+      }
+    ]
+
+  }, {
+
+    keywords: 'good evening',
+    reply: [
+      {
+        replyWord: 'Evening!, how is the weather today?'
+      }, {
+        replyWord: 'Good Evening!, how can i assist you today'
+      }, {
+        replyWord: 'Good Evening!, how may i be of service'
+      }, {
+        replyWord: "Evening!, what can i do for you"
+      }
+    ]
+
+  }, {
+
+    keywords: 'Good evening',
+    reply: [
+      {
+        replyWord: 'Evening!, how is the weather today?'
+      }, {
+        replyWord: 'Good Evening!, how can i assist you today'
+      }, {
+        replyWord: 'Good Evening!, how may i be of service'
+      }, {
+        replyWord: "Evening!, what can i do for you"
+      }
+    ]
+
+  }, {
+
+    keywords: 'good night',
+    reply: [
+      {
+        replyWord: 'Good Night!, sleep tight'
+      }, {
+        replyWord: 'Good Night!, sweet dreams'
+      }, {
+        replyWord: 'Good Night!, sleep well okay'
+      }, {
+        replyWord: "Alright!, see you tomorrrow"
+      }
+    ]
+
+  }, {
+
+    keywords: 'Good night',
+    reply: [
+      {
+        replyWord: 'Good Night!, sleep tight'
+      }, {
+        replyWord: 'Good Night!, sweet dreams'
+      }, {
+        replyWord: 'Good Night!, sleep well okay'
+      }, {
+        replyWord: "Alright!, see you tomorrrow"
+      }
+    ]
+
+  }
+]
