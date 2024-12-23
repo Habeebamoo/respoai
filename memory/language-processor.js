849f3a780197732569f05361cad77c238c4c919b
Habeeb Amoo <158174333+Habeebamoo@users.noreply.github.com>
2024-12-23 17:32:00 +0100

    Update language-processor.js

--- memory/language-processor.js
+++ memory/language-processor.js
@@ -20,7 +20,7 @@ export function getTimeStage() {
 export const neurons = [
   {
     input: ['hello', 'Hello', 'hi', 'Hi', 'hey', 'Hey', 'how far', 'How far', 'howfar', 'Howfar', 'hafar', 'Hafar', 'awfa', 'Awfa', 'awfar', 'Awfar', 'afar', 'Afar', 'hey man', 'Hey man'],
-    output: ['Hi there!', 'How may i be of help', 'Hi, What can i help you with', 'Hey there!, how man i be of help', "I'am good, you?", 'Am active', 'Good how may i be of service', 'Good, anything?']
+    output: ['Hi there!', 'How may i be of help', 'Hi, What can i help you with', 'Hey there!, how may i be of help', "I'am good, you?", 'Am active', 'Good how may i be of service', 'Good, anything?']
   }, {
     input: ['how are you', 'How are you', 'how are you doing', 'How are you doing'],
     output: ["I'am fine and you", "Good and you?", "I am fine"]
@@ -40,7 +40,7 @@ export const neurons = [
     input: ['good day', 'Good day', 'Good Day'],
     output: ['Thanks, my day is going fine, yours?', 'Okay thanks, any help?']
   }, {
-    input: ['what are you', 'What are you', 'who are you', 'Who are you', 'what is your name', 'What is your name', 'who created you', 'Who created you', 'who is your creator', 'Who is your creator', 'who invented you', 'Who invented you', 'who is your inventor', 'Who is your inventor', "what's your name", "What's your name"],
+    input: ['what are you', 'What are you', 'who are you', 'Who are you', 'what is your name', 'What is your name', 'who created you', 'Who created you', 'who made you', 'Who made you', 'who is your creator', 'Who is your creator', 'who invented you', 'Who invented you', 'who is your inventor', 'Who is your inventor', "what's your name", "What's your name"],
     output: [
       `My name is Respo AI, i was built by <pre>Dev. Habeeb O. Amoo</pre> to respond to humans chat <p>though i am not highly sophisticated like other AI but i was built in a complex system.</p>My main process is to use Natural Language Processing (NLP) to understand your message and then use Data structuring to give you a reply just like normal humans do. NLP represents your native language while data structuring is just all what you've learnt.<pre>so does this answers your questions?</pre>`
     ]
