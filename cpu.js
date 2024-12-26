4b717de3d0b51727e3c04519294de48ea6cf00cc
Habeeb Amoo <158174333+Habeebamoo@users.noreply.github.com>
2024-12-26 22:32:06 +0100

    Update cpu.js

--- cpu.js
+++ cpu.js
@@ -21,7 +21,7 @@ function windowRunCommand(event, prompt) {
 }
 
 const myNumbers = [0,1,2,3,4,5,6,7,8,9];
-const aiResponse = '';
+let aiResponse = '';
 
 function generateResponse(prompt) {
   let structuredData;
