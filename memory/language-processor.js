79ac76a68cd4c4e7b60f46a7a5073c4948bbd8aa
Habeeb Amoo <158174333+Habeebamoo@users.noreply.github.com>
2024-12-26 22:08:35 +0100

    Update language-processor.js

--- memory/language-processor.js
+++ memory/language-processor.js
@@ -36,7 +36,7 @@ export const neurons = [
     ]
   }, {
     input: ['what is the time', 'what is time', 'what does the time say', "what does the time say's", "what say's the time", 'what says the time', "what's the time", 'what time is it'],
-    output: [`The time is ${now.getHours()}:${now.getMinutes()}${meridian}`]
+    output: [`The time says ${now.getHours()}:${now.getMinutes()}${meridian}`, `The time is ${now.getHours()}:${now.getMinutes()}${meridian}`, `${now.getHours()}:${now.getMinutes()}${meridian} says the time`]
   }, {
     input: ['tell me a fact', 'suprise me with something interesting', 'tell me something interesting', 'suprise me'],
     output: ["Did you know that honey never spoils?", "Octopuses have three hearts!", "Bananas are berries, but strawberries aren’t."]
